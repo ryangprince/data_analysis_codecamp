5e5380434c26e928647967e744e6c5e0aa4c766a
ryangprince <rpprince18@gmail.com>
2024-10-31 15:12:54 -0400

    new file:   data/raw/Ecommerce_Customers.csv new file:   data/raw/percent-bachelors-degrees-women-usa.csv new file:   fcc_py_for_data_sci.py new file:   lin_reg_alejandro_ao.py new file:   log_reg_alejandro_ao.py new file:   notebooks/fcc_py_for_data_sci.ipynb

---
+++ log_reg_alejandro_ao.py
@@ -0,0 +1,8 @@
+# PRACTICE PROJECT
+
+# * Logistic Regression Project: Cancer Prediction with Python
+# * Link to YouTube: https://youtu.be/My4JgIeFdWk?si=FOA8iMtV99Zo3CwR
+# * Link to kaggle data set: https://www.kaggle.com/datasets/uciml/breast-cancer-wisconsin-data
+
+
+
